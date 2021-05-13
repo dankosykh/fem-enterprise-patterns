0699c881c408a88d5e595d49551bd60ce9be009d
Daniel Kosykh <dankosykh@gmail.com>
2021-05-12 22:23:24 -0700

    start

--- apps/micro/src/app/home/home.component.ts
+++ apps/micro/src/app/home/home.component.ts
@@ -12,23 +12,39 @@ export class HomeComponent {
   mode;
   widgets: Widget[];
 
-  updateWidgetsAndRecalculateCost(widget: Widget) {
-    switch (this.mode) {
+  reCalculateTotal(mode: string, widgets: Widget[], widget: Widget) {
+    this.widgets = this.updateWidgets(mode, widgets, widget);
+    this.price =  this.getTotalPrice(widgets);
+  }
+
+  updateWidgets(mode: string, widgets: Widget[], widget: Widget) {
+    switch (mode) {
       case 'create':
-        const newWidget = Object.assign({}, widget, { id: uuidv4() });
-        this.widgets = [...this.widgets, newWidget];
-        break;
+       return this.addWidget(widgets, widget)
       case 'update':
-        this.widgets = this.widgets.map(_widget => widget.id === _widget.id
-          ? Object.assign({}, widget) : _widget);
-        break;
+        return this.updateWidget(widgets, widget);
       case 'delete':
-        this.widgets = this.widgets.filter(_widget => widget.id !== _widget.id);
-        break;
+        return this.deleteWidget(widgets, widget);
       default:
         break;
     }
-    return this.widgets.reduce((acc, curr) => acc + curr.price, 0);
   }
 
+  updateWidget(widgets, widget) {
+    return widgets.map(_widget => widget.id === _widget.id
+      ? Object.assign({}, widget) : _widget);
+  }
+
+  addWidget(widgets, widget) {
+    const newWidget = Object.assign({}, widget, { id: uuidv4() });
+    return [...widgets, newWidget];
+  }
+
+  deleteWidget(widgets, widget) {
+    return widgets.filter(_widget => widget.id !== _widget.id);
+  }
+
+  getTotalPrice(widgets) {
+    return widgets.reduce((acc, curr) => acc + curr.price, 0);
+  }
 }
